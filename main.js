9e4988cbb8cecf2bf41c1f6e6862e8cd12328bcc
luoyisen <1402175410@qq.com>
2025-07-01 09:49:43 +0800

    fix2

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log("This is a test file for SCM input handling.");
 console.log("This is a test file for SCM input handling.1");
+console.log("This is a test file for SCM input handling.2");
