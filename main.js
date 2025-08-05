f685b05139fb0bac8f3bf86e49bfd69548860015
luoyisen <1402175410@qq.com>
2025-08-05 10:41:35 +0800

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 console.log("This is a test file for SCM input handling.");
 console.log("This is a test file for SCM input handling.1");
 console.log("This is a test file for SCM input handling.2");
-console.log("hello xiaojian")
+console.log("hello xiaojian ")
